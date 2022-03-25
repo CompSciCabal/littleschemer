f464e180226108912d16da86fe1db6d2b20f738a
dann <gitlab@mrmuster.com>
2022-03-24 21:19:19 -0400

    final commit!

--- scratch.rkt
+++ scratch.rkt
@@ -416,13 +416,13 @@
       [(eq? x 'x) mult]
       [else exp])))
 
-(define value
+(define value1
   (lambda (nexp)
     (cond
       [(atom? nexp) nexp]
       [else
-       ((atom-to-function (operator nexp)) (value (1st-sub-exp nexp))
-                                           (value (2nd-sub-exp nexp)))])))
+       ((atom-to-function (operator nexp)) (value1 (1st-sub-exp nexp))
+                                           (value1 (2nd-sub-exp nexp)))])))
 
 (define multirember-f
   (lambda (test?)
@@ -762,3 +762,152 @@
       [(eq? (car l) 'lambda) *lambda]
       [(eq? (car l) 'cond) *cond]
       [else *application])))
+
+(define *const
+  (lambda (e table)
+    (cond
+      ((number? e) e)
+      ((eq? e #t) #t)
+      ((eq? e #f) #f)
+      (else (build (quote primitive) e)))))
+
+(define *quote
+  (lambda (e table)
+    (text-of e)))
+
+(define text-of
+  (lambda (e)
+    (cadr e)))
+
+(define *identifier
+  (lambda (e table)
+    (lookup-in-table e table initial-table)))
+
+(define initial-table
+  (lambda (name)
+    (car '())))
+
+(define *lambda
+  (lambda (e table)
+    (build 'non-primitive
+           (cons table (cdr e)))))
+
+(define expression-to-action
+  (lambda (e)
+    (cond
+      ((atom? e) (atom-to-action e))
+      (else (list-to-action e)))))
+
+(define meaning
+  (lambda (e table)
+    ((expression-to-action e) e table)))
+
+(define value
+  (lambda (e)
+    (meaning e (quote ()))))
+
+(define table-of first)
+(define formals-of second)
+(define body-of third)
+
+(define evcon
+  (lambda (lines table)
+    (cond
+      ((else? (question-of (car lines)))
+       (meaning (answer-of (car lines))
+                table ) )
+      ((meaning (question-of (car lines))
+                table )
+       (meaning (answer-of (car lines))
+                table))
+      (else (evcon (cdr lines) table)))))
+
+(define else?
+  (lambda (x)
+    (eq? x 'else)))
+
+(define question-of first)
+(define answer-of second)
+
+(define *cond
+  (lambda (e table)
+    (evcon (cdr e) table)))
+
+(define evlis
+  (lambda (args table)
+    (cond
+      ((null? args) '())
+      (else (cons (meaning (car args) table)
+                  (evlis (cdr args) table))))))
+
+(define *application
+  (lambda (e table)
+    (apply
+     (meaning (function-of e) table)
+     (evlis (arguments-of e) table))))
+
+(define function-of car)
+(define arguments-of cdr)
+
+(define primitive?
+  (lambda (l)
+    (eq? (first l) 'primitive)))
+
+(define non-primitive?
+  (lambda (l)
+    (eq? (first l) 'non-primitive)))
+
+(define apply
+  (lambda (fun vals)
+    (cond
+      ((primitive? fun)
+       (apply-primitive (second fun) vals))
+      ((non-primitive? fun)
+       (apply-closure (second fun) vals)))))
+       
+(define apply-primitive
+  (lambda (name vals)
+    (cond
+      ((eq? name 'cons )
+       (cons (first vals) (second vals)))
+      ((eq? name (quote car))
+       (car (first vals)))
+      ((eq? name (quote cdr))
+       (cdr (first vals)))
+      ((eq? name (quote null?))
+       (null? (first vals)))
+      ((eq? name (quote eq?))
+       (eq? (first vals) (second vals)))
+      ((eq? name (quote atom?))
+       (:atom? (first vals)))
+      ((eq? name (quote zero?))
+       (zero? (first vals)))
+      ((eq? name (quote add1))
+       (add1 (first vals)))
+      ((eq? name (quote sub1))
+       (sub1 (first vals)))
+      ((eq? name (quote number?))
+       (number? (first vals))))))
+
+(define :atom?
+  (lambda (x)
+      (cond
+        ((atom? x #t))
+        ((null? x #f))
+        ((eq? (car x) (quote primitive))
+         #t )
+        ((eq? (car x) (quote non-primitive))
+         #t )
+        (else #f))))
+
+; (meaning (fancy-thing (second (second fun)) vals (third (second fun))) table)))))
+
+(define apply-closure
+  (lambda (closure vals)
+    (meaning (body-of closure)
+             (extend-table
+              (new-entry (formals-of closure) vals )
+              (table-of closure)))))
+
+
+
